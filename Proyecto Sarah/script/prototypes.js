dbe5546ddb09e2c1a863394cb57d97437a800fee
Sarah <109755587+SarahFlorez03@users.noreply.github.com>
2022-10-24 23:14:44 -0300

    Delete Proyecto Sarah directory

--- Proyecto Sarah/script/prototypes.js
+++
@@ -1,9 +0,0 @@
-/* Adds Element BEFORE NeighborElement */
-Element.prototype.appendBefore = function(element) {
-    element.parentNode.insertBefore(this, element);
-  }, false;
-  
-/* Adds Element AFTER NeighborElement */
-Element.prototype.appendAfter = function(element) {
-element.parentNode.insertBefore(this, element.nextSibling);
-}, false;
